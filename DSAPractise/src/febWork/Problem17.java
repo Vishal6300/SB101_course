9151138e5cf2003445fa74183d8c697e1c6567cf
Vishal6300 <vkdeoghar1998@gmail.com>
2023-02-22 23:25:18 +0530

    added DSA question and answers

---
+++ DSAPractise/src/febWork/Problem17.java
@@ -0,0 +1,74 @@
+package febWork;
+
+import java.util.Scanner;
+
+/*
+ We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+ */
+public class Problem17 {
+	 public static String getSmallestAndLargest(String s, int k) {
+	        String smallest = s.substring(0, k);
+	        String largest = "";
+	        
+	       
+	        for(int i=0;i<s.length()-k+1;i++){
+	           String str=s.substring(i, i+k); 
+	           if(str.compareTo(largest)>0){
+	               largest=str;
+	           }else if(str.compareTo(smallest)<0){
+	               smallest=str;
+	               
+	           }
+	            
+	        }
+	        // Complete the function
+	        // 'smallest' must be the lexicographically smallest substring of length 'k'
+	        // 'largest' must be the lexicographically largest substring of length 'k'
+	        
+	        return smallest + "\n" + largest;
+	    }
+
+
+	    public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        String s = scan.next();
+	        int k = scan.nextInt();
+	        scan.close();
+	      
+	        System.out.println(getSmallestAndLargest(s, k));
+	    }
+}
